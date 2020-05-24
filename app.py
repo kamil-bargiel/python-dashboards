f8aa05393d890f22d07d0aba022025f049414786
Kamil Bargiel <kamil.bargiel@relayr.io>
2020-05-24 16:30:18 +0200

    import data, add navi bar

--- app.py
+++ app.py
@@ -3,11 +3,44 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import flask
+import numpy as np
+import pandas as pd
+import dash_bootstrap_components as dbc
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 server = flask.Flask(__name__)
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server=server)
+
+death_df =  pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
+confirmed_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+recovered_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
+country_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv')
+
+death_df.rename(columns={'Country/Region': 'Country'}, inplace=True)
+confirmed_df.rename(columns={'Country/Region': 'Country'}, inplace=True)
+recovered_df.rename(columns={'Country/Region': 'Country'}, inplace=True)
+country_df = country_df.rename(columns={'Country_Region': 'Country', 'Long_': 'Long'}, inplace=True)
+
+death_df.drop('Province/State', axis=1, inplace=True)
+confirmed_df.drop('Province/State', axis=1, inplace=True)
+recovered_df.drop('Province/State', axis=1, inplace=True)
+
+country_df.sort_values('Confirmed', ascending=False, inplace=True)
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(html.A("Daily Data", href="#nav-daily-graph",            style = {'color': '#fff'}), className="mr-5"),
+        dbc.NavItem(html.A("Most effected", href="#nav-top-country-graph", style = {'color': '#fff'}), className="mr-5"),
+        dbc.NavItem(html.A("Comparison", href="#nav-cr-link", style = {'color': '#fff'}), className="mr-5"),
+    ],
+    brand="COVID-19",
+    brand_href="/",
+    color="dark",
+    dark=True,
+    className="p-3 fixed-top"
+)
+
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
